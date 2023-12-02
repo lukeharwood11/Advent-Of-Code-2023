15b851fc1fd50bef03a9622189c6aaf7f5773fcc
Luke Harwood <lukeharwood11@gmail.com>
2023-12-02 13:05:20 -0600

    Day 2 Solution

--- python/day2/solution.py
+++ python/day2/solution.py
@@ -4,23 +4,26 @@ def main():
     with open("./input.txt", 'r') as f:
         text = f.read()
     reg = re.compile(r"Game (\d+): (.*)")
-    options = [('red', 12), ('green', 13), ('blue', 14)]
+    options = {
+        'red': 12,
+        'green': 13,
+        'blue': 14
+    }
     sum = 0
     # each game
     for line in text.splitlines():
         match = reg.match(line)
-        print(line)
         _id = match.group(1)
         values = match.group(2)
         # for each round
         valid = True
         for semicolon_sep in values.split(";"):
             for pair in semicolon_sep.split(","):
-                _num = re.search(r"(\d+)", pair)
+                _num = re.search(r"(\d+) ([a-z]+)", pair)
                 _value = int(_num.group(1))
-                for value, max in options:
-                    if value in pair and _value > max:
-                        valid = False
+                _color = _num.group(2)
+                if _value > options[_color]:
+                    valid = False
         if valid:
             sum += int(_id)
     print(sum)
--- python/day2/solution2.py
+++ python/day2/solution2.py
@@ -4,21 +4,24 @@ def main():
     with open("./input.txt", 'r') as f:
         text = f.read()
     reg = re.compile(r"Game (\d+): (.*)")
-    options = [('red', 12), ('green', 13), ('blue', 14)]
     sum = 0
     # each game
     for line in text.splitlines():
         match = reg.match(line)
         values = match.group(2)
         # for each round
-        min_map = { value: 0 for (value, _) in options }
+        min_map = {
+            'red': 0,
+            'green': 0,
+            'blue': 0
+        }
         for semicolon_sep in values.split(";"):
             for pair in semicolon_sep.split(","):
-                _num = re.search(r"(\d+)", pair)
+                _num = re.search(r"(\d+) ([a-z]+)", pair)
                 _value = int(_num.group(1))
-                for value, _ in options:
-                    if value in pair and min_map[value] < _value:
-                        min_map[value] = _value 
+                _color = _num.group(2)
+                if min_map[_color] < _value:
+                    min_map[_color] = _value
 
         power = reduce(lambda acc, v: acc * v, min_map.values())
         sum += power
